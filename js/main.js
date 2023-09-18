4440d10cee82130aa27f3b747a32178a9ff433d7
Isadora <106935739+guatxinim@users.noreply.github.com>
2023-09-18 09:27:02 -0300

    alterando js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,24 @@
 const content = document.getElementById("content")
 
+const createSection = () => {
+    res.characters.forEach(element => {
+        const section = document.createElement("section");
+        fetch(element)
+        .then(res => res.json())
+        .then(res => {
+            h2Name = document.createElement("h2");
+            gender = document.createElement("p");
+            birth = document.createElement("p");
+            homeworld = document.createElement("p");
+        })
+        content.appendChild(section);
+    });
+}
+
 fetch("https://swapi.dev/api/films/1/")
 .then(res => res.json())
 .then(res => {
-    const h1 = document.createElement("h1");
-    h1.innerText = res.title
-    content.appendChild(h1)
+    const h1 = document.getElementById("filmTitle");
+    h1.innerText = res.title;
+    content.appendChild(createSection(res));
 })
